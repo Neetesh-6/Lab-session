2a50c9652bd968c0b3f2594f785e96713182d138
Neetesh-6 <114156149+Neetesh-6@users.noreply.github.com>
2022-09-29 17:37:21 +0530

    Add files via upload

---
+++ Findcurrentversion.java
@@ -0,0 +1,12 @@
+package CoreJava;
+
+public class Findcurrentversion {
+
+	public static void main (String args[])
+	{
+		System.out.println("version of java is :" +System.getProperty("java.version"));
+	}
+
+}
+
+
