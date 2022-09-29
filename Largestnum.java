02fc511e1b9801dc285cad36823fa54bde2b2cf4
Neetesh-6 <114156149+Neetesh-6@users.noreply.github.com>
2022-09-29 19:42:55 +0530

    Add files via upload

---
+++ Largestnum.java
@@ -0,0 +1,27 @@
+package CoreJava;
+import java.util.Scanner;
+public class Largestnum 
+
+{
+
+	public static void main(String[] args) {
+		// creating Scanner class
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first value :");
+		int num1 = sc.nextInt();
+		System.out.println("Enter second value :");
+		int num2 = sc.nextInt();
+		
+		// using  opration for largest value
+		if(num1>= num2)
+		{
+		System.out.println("the largest value :"+num1);
+		}
+		else
+		{
+			System.out.println("the largest value :"+num2);
+		}
+
+
+	}
+}
