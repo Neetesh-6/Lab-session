0eb7b482ce07279917ea6771acb45c8f6f07f70f
Neetesh-6 <114156149+Neetesh-6@users.noreply.github.com>
2022-10-28 15:40:47 +0530

    Add files via upload

---
+++ Happydiwali.java
@@ -0,0 +1,16 @@
+package CoreJava;
+import java.util.Scanner;
+public class Happydiwali {
+	public static void main(String[] args)
+	{
+	   Scanner sc= new Scanner (System.in);
+	  System.out.println("Enter n times you want to print the message:");
+	   int n=sc.nextInt();
+	   int i=0; 
+	      while(i<n)
+	      {
+	    	  System.out.println(" Wish you Happy Diwali 💥💥 ");
+	    	  i++;
+	      }
+	 }
+}
