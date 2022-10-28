c800e8bf1b45ea70e400dffe2d7fe6b63b45fd15
Neetesh-6 <114156149+Neetesh-6@users.noreply.github.com>
2022-10-28 15:42:09 +0530

    Add files via upload

---
+++ arrayshot.java
@@ -0,0 +1,17 @@
+package CoreJava;
+import java.util.Scanner;
+import java.util.Arrays;
+public class arrayshot {
+	 public static void main(String[] args)
+			{
+				Scanner sc = new Scanner(System.in);
+			    System.out.println("enter a string");
+			   String arr=sc.nextLine();
+			   char a[]=arr.toCharArray();
+			   System.out.println("reverse of a string is");
+		         int i=a.length;
+		         for(int j=i;j>0;j--)
+		         {
+		        	 System.out.print(a[j-1]);
+		         }
+		     }}
